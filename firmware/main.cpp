4fc9259fbe2c8f2924179f869afc7e37c6092678
Nathan Robinson <nrobinson2000@me.com>
2016-04-03 17:49:46 +0100

    at 17:49 of 03-04-2016

---
+++ firmware/main.cpp
@@ -0,0 +1,48 @@
+#include "application.h"
+#include "LiquidCrystal.h"
+
+// Make sure to update these to match how you've wired your pins.
+// pinout on LCD [RS, EN, D4, D5, D6, D7];
+// pin nums LCD  [ 4,  6, 11, 12, 13, 14];
+// Shield Shield [RS, EN, D4, D5, D6, D7];
+// Spark Core    [D3, D5, D2, D4, D7, D8];
+LiquidCrystal lcd(D0, D1, C3, C2, C1, C0);
+
+int LDR = A0;
+int brightness = 0;
+
+int printHandler(String args)
+{
+lcd.clear();
+ lcd.setCursor(0, 0);
+for (unsigned int i = 0; i < args.length(); i++)
+{
+  if (i >= 16)
+  {
+  lcd.setCursor(i - 16 ,1);
+  lcd.print(args.charAt(i));
+  }
+  else
+  {
+    lcd.setCursor(i, 0);
+    lcd.print(args.charAt(i));
+  }
+}
+
+return 1;
+}
+
+void setup()
+{
+pinMode(LDR, INPUT);
+Serial.begin(9600);
+
+Particle.function("print", printHandler);
+lcd.begin(16,2);
+
+}
+
+void loop()
+{
+
+}
